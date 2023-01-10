d7ae4b174db1c640105ef3f0eaf28f4efd5a2d89
Sitpi Rajendran <39497470+SitpiRajendran@users.noreply.github.com>
2023-01-10 19:30:56 +0100

    Add files via upload

--- recap.js
+++ recap.js
@@ -2,10 +2,13 @@ import fs from 'fs';
 
 //get today day of the week
 var today = new Date().getDay();
+var date = new Date();
+
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
 var realLate = [];
 var three_min_Late = [];
-console.log(weekday[today])  
+
 
 //function that get all json file in the folder and return an array of the file name
 function getTrains(typeOfTrain) {
@@ -16,7 +19,7 @@ function getTrains(typeOfTrain) {
             readFile(file)['trains'].forEach(train => {
                 if (!jsonFiles.some(el => el.id === train.id)) {
                     jsonFiles.push(train);
-                    if (train.difference.includes("minutes") && train.difference.includes("retard")){
+                    if (train.difference.includes("minutes") && train.difference.includes("retard")) {
                         realLate.push(train)
                         if (!train.difference.includes("1 minutes") && !train.difference.includes("2 minutes")) {
                             three_min_Late.push(train)
@@ -26,7 +29,6 @@ function getTrains(typeOfTrain) {
             });
         }
     });
-//    console.log(jsonFiles)
     return jsonFiles;
 }
 
@@ -36,11 +38,16 @@ function readFile(file) {
     return json;
 }
 
+var all_trains = getTrains("train_");
+var weird_trains = getTrains("weird_");
 
-
-const weird_trains = getTrains("weird_")
-
-console.log("Train Passé : " + getTrains("train_").length)
-console.log("Train +1 minutes de retard: " + realLate.length)
+console.log(weekday[today] + " " + date.getDate() + " " + date.getMonth() + " " + date.getFullYear()) 
+console.log("-")
+console.log("Trains passés à Roissy en Brie : " + all_trains.length)
+console.log("avec +1 minutes de retard: " + realLate.length)
 console.log("dont avec +3 minutes de retard: " + three_min_Late.length)
+console.log("")
 console.log("Train officiellement retardé ou annulé : " + weird_trains.length)
+weird_trains.forEach(train => {
+    console.log("    " + train.mission + " - " + train.destination + " : " + train.arrivee + " // " +  train.status)
+})
